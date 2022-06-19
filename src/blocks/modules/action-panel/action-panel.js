4ffac2212b0bcefb412c2285db0a9b7bc2a29401
Andrey Brekotkin <brekot@ya.ru>
2022-06-19 23:45:43 +0300

    First step

---
+++ src/blocks/modules/action-panel/action-panel.js
@@ -0,0 +1,13 @@
+modules.define('action-panel', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
---
+++ src/blocks/modules/work-area/work-area.js
@@ -0,0 +1,13 @@
+modules.define('work-area', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
